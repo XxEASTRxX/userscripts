5fb973a08ac0a93eb186af41d8cb88384f2ed38b
XxEASTRxX <65164097+XxEASTRxX@users.noreply.github.com>
2025-01-15 22:13:34 +0300

    Update eas.user.js

--- eas.user.js
+++ eas.user.js
@@ -4,7 +4,191 @@
 // @version      2.0
 // @description  Bypass ad-links using the EASx API
 // @author       EASx
-// @match        *://*/*
+// @match         *://mega-guy.com/*
+// @match         *://loot-link.com/*
+// @match         *://best-links.org/*
+// @match         *://loot-links.com/*
+// @match         *://megaspremium.com/*
+// @match         *://lootdest.com/*
+// @match         *://direct-links.net/*
+// @match         *://risquemega.com/*
+// @match         *://onlyfriends.club/*
+// @match         *://onepiecered.co/*
+// @match         *://multileaks.com/*
+// @match         *://luvsquad-links.com/*
+// @match         *://lootdest.org/*
+// @match         *://free-leaks.com/*
+// @match         *://goldmega.online/*
+// @match         *://realiukzemydre.com/*
+// @match         *://kmendation.com/*
+// @match         *://lootlinks.co/*
+// @match         *://onlyshare.info/*
+// @match         *://leakplugs.com/*
+// @match         *://nswfbox.com/*
+// @match         *://eofmukindwo.com/*
+// @match         *://op-packs.com/*
+// @match         *://thedetective.online/*
+// @match         *://forlinkmeg.com/*
+// @match         *://thegoatpack.org/*
+// @match         *://of-leaks.xyz/*
+// @match         *://links.spacebin.in/*
+// @match         *://supermeg.com/*
+// @match         *://onlyfanessereloaded.com/*
+// @match         *://fanzleaks.com/*
+// @match         *://cemendemons.com/*
+// @match         *://premiummegaz.com/*
+// @match         *://thepremium.online/*
+// @match         *://pasteebins.com/*
+// @match         *://baddiezcentral.com/*
+// @match         *://megazone.website/*
+// @match         *://leak-pragmatic.com/*
+// @match         *://megadumpz.com/*
+// @match         *://thhaven.net/*
+// @match         *://meg4fans.com/*
+// @match         *://depravityweb.co/*
+// @match         *://discordlink.cc/*
+// @match         *://megashub.co/*
+// @match         *://streamergirls.org/*
+// @match         *://your-leaks.com/*
+// @match         *://onlylinksmegas.xyz/*
+// @match         *://pypy.spacebin.in/*
+// @match         *://pypy.in/*
+// @match         *://luvsquad-links.cmo/*
+// @match         *://beast.net.in/*
+// @match         *://hook.beast.net.in/*
+// @match         *://all-fans.online/*
+// @match         *://fansmega.com/*
+// @match         *://worldpacks.co/*
+// @match         *://night-hub.online/*
+// @match         *://dailyofleaks.com/*
+// @match         *://pkofs.com/*
+// @match         *://offree90.com/*
+// @match         *://megadropz.com/*
+// @match         *://onlyfunlink.com/*
+// @match         *://direct-links.org/*
+// @match         *://leaks4you.com/*
+// @match         *://onlylinksmegas.com/*
+// @match         *://direct-link.net/*
+// @match         *://downbadleaks.com/*
+// @match         *://onlyfanscloud.com/*
+// @match         *://missleakz.com/*
+// @match         *://leakszone.online/*
+// @match         *://links-loot.com/*
+// @match         *://holyfanslinks.com/*
+// @match         *://utopianleaks.com/*
+// @match         *://megavip.store/*
+// @match         *://drlinker.com/*
+// @match         *://baddiesheaven.com/*
+// @match         *://of4lm-links.com/*
+// @match         *://holedonly.store/*
+// @match         *://lootlinks.com/*
+// @match         *://free-content.pro/*
+// @match         *://milky-center.com/*
+// @match         *://megaofs.com/*
+// @match         *://bleleadersto.com/*
+// @match         *://link-target.org/*
+// @match         *://daughablelea.com/*
+// @match         *://mymegalinks.com/*
+// @match         *://heroslut.com/*
+// @match         *://tonordersitye.com/*
+// @match         *://vip-linknetwork.com/*
+// @match         *://birdbiss.com/*
+// @match         *://loot-labs.com/*
+// @match         *://lootlabs.com/*
+// @match         *://link-hub.net/*
+// @match         *://locconnect.com/*
+// @match         *://premiumstashdrop.com/*
+// @match         *://lootdest.info/*
+// @match         *://of-area.com/*
+// @match         *://link-target.net/*
+// @match         *://megalnk.com/*
+// @match         *://lootlink.org/*
+// @match         *://nsfwcherry.com/*
+// @match         *://cherrypacks.online/*
+// @match         *://hotstars-leaks.com/*
+// @match         *://onlymega.co/*
+// @match         *://hanimeturks.com/*
+// @match         *://dailyleakz.com/*
+// @match         *://content-hub.club/*
+// @match         *://dailyadultmegas.com/*
+// @match         *://oui-chu.com/*
+// @match         *://megalinks.one/*
+// @match         *://starleakz.com/*
+// @match         *://babeslink.click/*
+// @match         *://ofgirls3x.com/*
+// @match         *://pnp-drops.me/*
+// @match         *://nsfw-paradise.club/*
+// @match         *://key-access.co/*
+// @match         *://leaksmix.com/*
+// @match         *://sensual-leaks.com/*
+// @match         *://ofhub-leaks.com/*
+// @match         *://onlyforfan.online/*
+// @match         *://sweetjuice-mega.com/*
+// @match         *://crip-hub.com/*
+// @match         *://leaksfreeday.com/*
+// @match         *://thotshaven.online/*
+// @match         *://mega-leaked.com/*
+// @match         *://beautifulgirls.social/*
+// @match         *://tavernleaks.com/*
+// @match         *://nudeleaksteenz.com/*
+// @match         *://slutywet.com/*
+// @match         *://leakutopia.site/*
+// @match         *://secret-packs.com/*
+// @match         *://attiktok22.com/*
+// @match         *://pancakes-leaks.com/*
+// @match         *://diamond-leaks.com/*
+// @match         *://lewd-leaks.com/*
+// @match         *://ftbleaks.net/*
+// @match         *://thepytheaven.org/*
+// @match         *://of-region.online/*
+// @match         *://admiregirls-byme.com/*
+// @match         *://mzehoney12.com/*
+// @match         *://herplace.online/*
+// @match         *://megaplugleaks.com/*
+// @match         *://of-kingdom.com/*
+// @match         *://onlyfansmegafolder.com/*
+// @match         *://links.spacebin.in/*
+// @match         *://adfoc.us/*
+// @match         *://boost.ink/*
+// @match         *://cuty.io/*
+// @match         *://www.cuty.io/*
+// @match         *://cety.app/*
+// @match         *://www.cety.app/*
+// @match         *://linkvertise.com/*/*
+// @match         *://mboost.me/*
+// @match         *://bst.gg/*
+// @match         *://booo.st/*
+// @match         *://mendationforc.info/*
+// @match         *://paster.so/*
+// @match         *://paster.gg/*
+// @match         *://rekonise.com/*
+// @match         *://social-unlock.com/*
+// @match         *://www.social-unlock.com/*
+// @match         *://socialwolvez.com/*
+// @match         *://sub2get.com/*
+// @match         *://www.sub2get.com/*
+// @match         *://sub2unlock.com/*
+// @match         *://sub2unlock.io/*
+// @match         *://sub2unlock.net/*
+// @match         *://sub2unlock.online/*
+// @match         *://sub2unlock.top/*
+// @match         *://sub4unlock.pro/*
+// @match         *://sub4unlock.com/*
+// @match         *://sub4unlock.io/*
+// @match         *://subfinal.com/*
+// @match         *://unlocknow.net/*
+// @match         *://v.gd/*
+// @match         *://work.ink/*
+// @match         *://www.work.ink/*
+// @match         *://workink.net/*
+// @match         *://r.work.ink/*
+// @match         *://workink.one/*
+// @match         *://workink.me/*
+// @match         *://ytsubme.com/*
+// @match         *://www.ytsubme.com/*
+// @match         *://esohasl.net/*
+// @match         *://rbscripts.net/*
+// @match         *://link.rbscripts.net/*
 // @downloadURL  https://raw.githubusercontent.com/XxEASTRxX/userscripts/refs/heads/main/newscript.js
 // @updateURL    https://raw.githubusercontent.com/XxEASTRxX/userscripts/refs/heads/main/newscript.js
 // @homepageURL  https://eas.lol
@@ -13,31 +197,11 @@
 // ==/UserScript==
 
 (async () => {
-    const supportedDomains = [
-    'linkvertise.com', 'link-target.net', 'link-center.net', 'link-to.net',
-    'bit.ly', 't.ly', 'jpeg.ly', 'tiny.cc', 'tinyurl.com', 'tinylink.onl',
-    'shorter.me', 'is.gd', 'v.gd', 'rebrand.ly', 'mboost.me', 'bst.gg',
-    'bst.wtf', 'boost.ink', 'sub2get.com', 'sub4unlock.io', 'sub4unlock.com',
-    'sub4unlock.net', 'rekonise.com', 'rkns.link', 'subfinal.com',
-    'unlocknow.net', 'ytsubme.com', 'work.ink', 'cuty.io', 'cuty.me',
-    'adfoc.us', 'justpaste.it', 'paste-drop.com', 'pastebin.com',
-    'pastecanyon.com', 'pastehill.com', 'pastemode.com', 'rentry.org',
-    'paster.so', 'loot-link.com', 'loot-links.com', 'lootlink.org',
-    'lootlinks.co', 'lootdest.info', 'lootdest.org', 'lootdest.com',
-    'links-loot.com', 'linksloot.net'
-];
-
 
     const config = {
         time: 10,
         endpoint: 'https://usr.eas-x.com'
     };
-
-    function isCurrentDomainSupported() {
-        const currentHostname = window.location.hostname.replace('www.', '');
-        return supportedDomains.some(domain => currentHostname === domain ||
-            currentHostname.endsWith('.' + domain));
-    }
 
     const urlParams = new URLSearchParams(window.location.search);
     const redirectUrl = urlParams.get('redirect');
@@ -57,11 +221,6 @@
             return;
         }
     }
-
-    if (!isCurrentDomainSupported()) {
-        return;
-    }
-
 
     Object.defineProperty(Document.prototype, 'referrer', {
         get: function() {
